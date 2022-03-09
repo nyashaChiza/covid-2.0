a26ab7a49f9fb892cf9b716bf2b680ecd8bb9889
Nyasha Chizampeni <nchizampeni@gmail.com>
2022-03-09 21:09:41 +0200

    hh

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, session
+from flask import Flask, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from covid import Covid
 
